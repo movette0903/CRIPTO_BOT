45488d949193bc15575e5e9543651653c1e1d39a
movette0903 <93793416+movette0903@users.noreply.github.com>
2023-06-16 21:16:19 +0300

    Delete Задание_12.7.3.py

--- Задание_12.7.3.py
+++
@@ -1,8 +0,0 @@
-money = int(input('Введите сумму вклада'))
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit = []
-for value in per_cent:
-    deposit.append(per_cent[value] * money / 100)
-max_deposit = max(deposit)
-print(deposit)
-print ("Максимальная сумма, которую вы можете заработать — " + str(max_deposit))
